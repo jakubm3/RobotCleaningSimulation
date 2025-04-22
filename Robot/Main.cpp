642065e4e44779c5b34278eace76f4ab09cb1e68
Gab-Master <Gabryel.jun@gmail.com>
2025-04-22 13:33:37 +0200

    Created project and started main file

---
+++ Robot/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 2) {
+		// No file given
+	}
+	else {
+		// Try to load file
+	}
+
+	std::cout << "\nCLEANING ROBOT SIMULATION\n\n\n";
+	// If file not loaded, give path
+	if (true) {
+		std::cout << "Give file path: ";
+		std::string givenPath;
+		std::cin >> givenPath;
+	}
+
+	// If file loaded succesfully, start simulation
+
+	return 0;
+}
