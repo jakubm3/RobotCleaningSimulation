f299f887999b691605c1cf4d8905d561a27f6a40
Gab-Master <Gabryel.jun@gmail.com>
2025-05-23 12:01:04 +0200

    Some methods for robot

---
+++ Robot/Robot.cpp
@@ -0,0 +1,91 @@
+#include "Robot.h"
+
+Robot::Robot(size_t mapWidth, size_t mapHeight, size_t chargerId) noexcept {
+	// TODO
+	// Load width x height map of unvisited tiles
+	// Explore chargerId
+	position_ = chargerId;
+	chargerId_ = chargerId;
+	currTask = RobotAction::explore;
+}
+
+void Robot::cleanTile() noexcept {
+	// TODO
+	// Check how dirty is tile, set efficiency
+	// Drains energy = cleaningEfficiency
+}
+
+std::tuple<RobotAction, Direction> Robot::makeAction() {
+	// Check current tile, clean if trash
+	cleanTile();
+	if (cleaningEfficiency > 0) {
+		return std::make_tuple(RobotAction::clean, Direction::none);
+	}
+	
+	// If has move target and in cleaning mode
+	if (!path.empty() && currTask == RobotAction::clean) {
+		// TODO
+		// If next target has trash, ignore
+		// Check tiles around, if has trash, go there
+	}
+	// If no move target and in explore mode
+	else if (currTask == RobotAction::explore) {
+		size_t nextTarget;
+		if (findUnvisited(nextTarget)) {
+			// Go towards next unvisited
+			createPath(nextTarget);
+		}
+		else {
+			// No unvisited, finished exploring
+			currTask = RobotAction::clean;
+		}
+	}
+	// If no move target and in moving mode
+	else if (currTask == RobotAction::move) {
+		currTask = RobotAction::clean;
+	}
+
+	// If no move target, go to nearest trash or home
+	if (path.empty()) {
+		size_t nextTarget;
+		if (findTrash(nextTarget)) {
+			// Found trash, go to trash
+			createPath(nextTarget);
+		}
+		else {
+			// No trash found, go home
+			// TODO EXTRA
+			// If in home, clean more efficiently
+			currTask = RobotAction::move;
+			createPath(chargerId_);
+		}
+	}
+
+	return std::make_tuple(RobotAction::move, move());
+}
+
+Direction Robot::move() {
+	// TODO
+	return Direction::none;
+}
+
+void Robot::createPath(size_t targetId) {
+	// TODO
+}
+
+bool Robot::findUnvisited(size_t& targetId) const {
+	// TODO
+	return true;
+}
+
+bool Robot::findTrash(size_t& targetId) const {
+	// TODO
+	return true;
+}
+
+void Robot::loadRobot(std::istream& in) {
+	// TODO
+}
+void Robot::saveRobot(std::ostream& out) const {
+	// TODO
+}
